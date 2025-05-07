98734a6582238aa6e5c784f50d87510471eebcb4
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-05-07 15:38:03 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FeaturesComponent} from './components/features/features.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import {HeroComponent} from './components/hero/hero.component';
+import {TestimonialsComponent} from './components/testimonials/testimonials.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeaturesComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeroComponent,
+    TestimonialsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
