b54071fdab4b42e6cab057f7e05300dca61ffde0
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-05-07 21:27:42 +0200

    Diseño de feature component

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-features',
@@ -6,6 +6,31 @@ import { Component } from '@angular/core';
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
-export class FeaturesComponent {
+export class FeaturesComponent implements OnInit {
+  ngOnInit(): void {
+    const elements = document.querySelectorAll('.gradient-border') as NodeListOf<HTMLElement>;
+    const colores =[
+       'red, yellow',
+       'yellow, green',
+       'green, blue',
+       'blue, purple',
+       'purple, red',
+       'red, yellow',
+      ]
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      // Establece el color inicial del borde
+      (element as HTMLElement).style.setProperty('background-image', `linear-gradient(black,black), linear-gradient(100deg, ${colores[i]})`);
+      element.addEventListener('mousemove', (event: MouseEvent) => {
+        const rect = element.getBoundingClientRect();
+        const x = event.clientX - rect.left; // Posición X relativa al elemento
+        const width = rect.width;
 
+        // Calcula la posición del gradiente para que el rojo siga al ratón
+        const percentage = 100 - (x / width) * 100; // Invertir la posición del rojo basada en el ratón // Posición del rojo basada en el ratón
+        (element as HTMLElement).style.setProperty('background-image', `linear-gradient(black,black), linear-gradient(${percentage}deg, ${colores[i]})`);
+        console.log(percentage);
+      });
+    }
+  }
 }
