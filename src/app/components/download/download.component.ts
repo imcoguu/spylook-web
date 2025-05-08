32dad995d02d35deddd30939fea879ac9f488ab1
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-05-08 10:46:22 +0200

    Nuevo Download component, mejor estilo en general en la página

---
+++ src/app/components/download/download.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download',
+  standalone: false,
+  templateUrl: './download.component.html',
+  styleUrl: './download.component.css'
+})
+export class DownloadComponent {
+
+}
